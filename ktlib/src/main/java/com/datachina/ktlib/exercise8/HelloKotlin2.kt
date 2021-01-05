d16f202c1a62e0148b6c086b3e76a4be70e8bac5
Chenstyle <lizhizhao81@gmail.com>
2021-01-05 21:58:11 +0800

    throw and annotation

---
+++ ktlib/src/main/java/com/datachina/ktlib/exercise8/HelloKotlin2.kt
@@ -0,0 +1,32 @@
+package com.datachina.ktlib.exercise8
+
+/**
+ * throw在Kotlin中是一个表达式，这样我们可以将throw作为Elvis表达式的一部分
+ * throw表达式的类型是一种特殊的类型：Nothing。
+ * 在自己的代码中，可以使用Nothing来标记永远不会返回的函数
+ */
+fun main() {
+    val str: String? = "a"
+    val str2 = str ?: throw IllegalArgumentException("值不能为空")
+
+    println(str2)
+
+    println("----------")
+
+    val str3 = str
+    println(str3)
+
+    println("----------")
+
+//    println(myMethod("message"))  // compile error: Nothing has no instances
+
+    var s = null
+    println(s is Nothing?)
+
+    var s2 = listOf(null)
+    println(s2 is List<Nothing?>)
+}
+
+fun myMethod(message: String): Nothing {
+    throw IllegalArgumentException(message)
+}
---
+++ ktlib/src/main/java/com/datachina/ktlib/exercise9/HelloKotlin1.kt
@@ -0,0 +1,28 @@
+package com.datachina.ktlib.exercise9
+
+/**
+ * 注解 (annotation)
+ * meta-annotation (元注解)
+ */
+
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION, AnnotationTarget.VALUE_PARAMETER,
+        AnnotationTarget.EXPRESSION, AnnotationTarget.CONSTRUCTOR, AnnotationTarget.PROPERTY_SETTER)
+@Retention(AnnotationRetention.SOURCE)
+@MustBeDocumented
+annotation class MyAnnotation
+
+@MyAnnotation
+class MyClass {
+
+    @MyAnnotation
+    fun myMethod(@MyAnnotation a: Int): Int {
+        return (@MyAnnotation 10)
+    }
+}
+
+class MyClass2 @MyAnnotation constructor(a: Int)
+
+class MyClass3 {
+    var a: Int? = null
+        @MyAnnotation set
+}
