35e7da94e53b1bc2caa506f8ddf67a397a3728d6
Chenstyle <lizhizhao81@gmail.com>
2021-01-06 22:55:06 +0800

    kotlin & java

---
+++ ktlib/src/main/java/com/datachina/ktlib/exercise10/Person.java
@@ -0,0 +1,35 @@
+package com.datachina.ktlib.exercise10;
+
+/**
+ * @auther to Chenstyle
+ * @time 2021/1/6 0006 22:08
+ */
+class Person {
+    private String name;
+    private boolean married;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isMarried() {
+        return married;
+    }
+
+    public void setMarried(boolean married) {
+        this.married = married;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
