6750b6f1b7fd71ef7f2b6170bfdcaef9bcb2bb91
Chenstyle <lizhizhao81@gmail.com>
2021-01-07 18:30:53 +0800

    java invoked kotlin

---
+++ ktlib/src/main/java/com/datachina/ktlib/exercise11/HelloKotlin3.kt
@@ -0,0 +1,10 @@
+@file: JvmName("HelloWorld")
+@file: JvmMultifileClass
+
+package com.datachina.ktlib.exercise11
+
+fun myPrint() {
+    println("hello world")
+}
+
+var myStr: String = "hello world"
---
+++ ktlib/src/main/java/com/datachina/ktlib/exercise11/HelloKotlin4.kt
@@ -0,0 +1,8 @@
+@file: JvmName("HelloWorld")
+@file: JvmMultifileClass
+
+package com.datachina.ktlib.exercise11
+
+fun myPrint2() {
+    println("myPrint2")
+}
