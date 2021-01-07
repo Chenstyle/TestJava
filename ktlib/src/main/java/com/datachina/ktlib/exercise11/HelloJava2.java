85b68b0f7f169d5896cd1bcba1e9cf2f596b126d
Chenstyle <lizhizhao81@gmail.com>
2021-01-07 18:21:42 +0800

    java invoked kotlin

---
+++ ktlib/src/main/java/com/datachina/ktlib/exercise11/HelloJava2.java
@@ -0,0 +1,16 @@
+package com.datachina.ktlib.exercise11;
+
+class HelloJava2 {
+    public static void main(String[] args) {
+        // 我们无法通过new关键字来创建Kotlin编译器自动生成的以Kt结尾的类的实例
+        // 因为在生成的字节码中没有不带参数的构造方法
+        HelloKotlin2Kt helloKotlin2Kt = new HelloKotlin2Kt();
+
+        MyClass myClass = new MyClass();
+
+        HelloKotlin2Kt.setStr("welcome");
+        System.out.println(HelloKotlin2Kt.getStr());
+
+        HelloKotlin2Kt.test();
+    }
+}
---
+++ ktlib/src/main/java/com/datachina/ktlib/exercise11/HelloKotlin2.kt
@@ -0,0 +1,12 @@
+package com.datachina.ktlib.exercise11
+
+/**
+ * 如果在一个文件当中编写了属性，方法，那么这个属性或者方法对应的是这个文件名的Kt.class的静态属性或方法
+ */
+class MyClass
+
+fun test() {
+    println("hello world")
+}
+
+var str: String = "hello"
