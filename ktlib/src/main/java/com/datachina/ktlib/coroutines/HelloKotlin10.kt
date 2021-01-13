aadaaba1ef80e049df046e6ab0659fe5d083447d
Chenstyle <lizhizhao81@gmail.com>
2021-01-13 23:57:47 +0800

    func

--- ktlib/src/main/java/com/datachina/ktlib/coroutines/HelloKotlin10.kt
+++ ktlib/src/main/java/com/datachina/ktlib/coroutines/HelloKotlin10.kt
@@ -1,24 +1,29 @@
 package com.datachina.ktlib.coroutines
 
 fun main() {
-    test(5, {
-        println("hello test")
-    })
     test(5) {
         println("hello test")
     }
-    test2(5, {
-        println("hello test2")
-    })
     test2(5) {
         println("hello test2")
     }
+    test(5, ::test3)
+    test2(5, ::test4)
+    test5(5, ::test6)
 }
 
-fun test(x: Int, action: () -> Unit) {
-    action()
+fun test(x: Int, action: () -> Unit) {}
+
+fun test2(x: Int, action: (Int) -> Unit) {}
+
+fun test3() {}
+
+fun test4(x: Int) {}
+
+fun test5(x: Int, action: (Int, Int) -> Unit) {
+    action(1, 2)
 }
 
-fun test2(x: Int, action: (Int) -> Unit) {
-    action(x)
+fun test6(x: Int, y: Int) {
+    println(x + y)
 }
