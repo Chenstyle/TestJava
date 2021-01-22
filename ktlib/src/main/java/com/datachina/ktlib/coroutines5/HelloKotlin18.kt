d76267fe14a8f2b7edd80b94879237841cb11a07
Chenstyle <lizhizhao81@gmail.com>
2021-01-22 15:43:18 +0800

    flow zip

---
+++ ktlib/src/main/java/com/datachina/ktlib/coroutines5/HelloKotlin18.kt
@@ -0,0 +1,17 @@
+package com.datachina.ktlib.coroutines5
+
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.zip
+import kotlinx.coroutines.runBlocking
+
+/**
+ *  Flow的组合
+ */
+fun main() = runBlocking {
+    val nums = (1..5).asFlow()
+    val strs = flowOf("one", "two", "three", "four", "five")
+
+    nums.zip(strs) { a, b -> "$a -> $b" }.collect { println(it) }
+}
