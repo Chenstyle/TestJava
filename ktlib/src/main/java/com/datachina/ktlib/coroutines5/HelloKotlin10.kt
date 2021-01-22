2872a0c0d6b206b77dc43b0fb71fd8ae3261957e
Chenstyle <lizhizhao81@gmail.com>
2021-01-22 15:04:38 +0800

    Terminal Operation

---
+++ ktlib/src/main/java/com/datachina/ktlib/coroutines5/HelloKotlin10.kt
@@ -0,0 +1,27 @@
+package com.datachina.ktlib.coroutines5
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.runBlocking
+
+/**
+ * 限定数量的中间操作
+ */
+private fun myNumbers(): Flow<Int> = flow {
+    try {
+        emit(1)
+        emit(2)
+        println("hello world")
+        emit(3)
+    } catch (ex: Exception) {
+        println(ex)
+    } finally {
+        println("finally")
+    }
+}
+
+fun main() = runBlocking {
+    myNumbers().take(2).collect { println(it) }
+}
